377bb58cb8be1030773e6af4e37bd3f1cbd4413b
Chad Martin <kurickabides@gmail.com>
2026-07-05 10:33:54 -0700

    update files

--- POC/northstar-utilities-demo/vite.config.ts
+++ POC/northstar-utilities-demo/vite.config.ts
@@ -14,6 +14,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
-  }
+    open: true,
+    port: 3000,
+  },
+  build: {
+    outDir: "dist",
+  },
 });
